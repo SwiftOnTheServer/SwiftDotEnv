febeca11d359b3026c88612b7bad78ccf2e6966a
Matt Dias <matt.dias6@gmail.com>
2018-01-27 17:36:17 -0500

    updated Package file to match Swift 4 specs

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "SwiftDotEnv",
     targets: [
-        Target(
+        .target(
             name: "DotEnv",
             dependencies: []
         ),
