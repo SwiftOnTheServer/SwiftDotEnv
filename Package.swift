0c16470dff5c85d0a1bfe6e24a48821449166bee
Matt Dias <matt.dias6@gmail.com>
2018-01-27 19:35:32 -0500

    removed deprecated use of `.characters` on a `String`

--- Package.swift
+++ Package.swift
@@ -4,10 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftDotEnv",
+    dependencies: [],
     targets: [
         .target(
             name: "DotEnv",
             dependencies: []
         ),
+        .testTarget(
+            name: "DotEnvTests",
+            dependencies: ["DotEnv"]
+        )
     ]
 )
